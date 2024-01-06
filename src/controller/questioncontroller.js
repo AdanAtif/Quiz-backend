49a1ab9bc26579ed42ed70b818cf3749e16df1f5
Adan <adanatif60@gmail.com>
2024-01-06 16:14:08 +0500

    Fixed Delete function

--- src/controller/questioncontroller.js
+++ src/controller/questioncontroller.js
@@ -1,194 +1,271 @@
-const prismadb = require("../config/db.js")
+const prismadb = require("../config/db.js");
 
 const addQuestion = async (req, res) => {
-    const {
-        correctOption,
-        id,
-        OptionOne,
-        OptionTwo,
-        OptionThree,
-        question
-    } = req.body
-    if (!correctOption || !OptionOne || !OptionTwo || !OptionThree || !id || !question) {
-        return res.send({
-            "status": "Failed",
-            "message": "please fill all field"
-        })
-        // checking if all fields are fields
-    }
-    if (OptionOne === OptionTwo || OptionOne === OptionThree || OptionTwo === OptionThree) {
-        return res.status(400).send({
-            "status": "Failed",
-            "message": "Options must be unique"
-        });
-        // Options should be different from each other
-    }
-    if (correctOption !== OptionOne && correctOption !== OptionTwo && correctOption !== OptionThree) {
-        return res.status(400).send({
-            "status": "Failed",
-            "message": "CorrectOption must be one of the provided options"
-        });
-        // Correct option must be same with one other option
-    }
-    try {
-        await prismadb.question.create({
-            data: {
-                quizId: id,
-                Question: question,
-                OptionOne: OptionOne,
-                OptionTwo: OptionTwo,
-                OptionThree: OptionThree,
-                CorrectOption: {
-                    create: {
-                        CorrectOption: correctOption,
-                    },
-                },
-            }
-        })
-        return res.status(204).send()
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error please try again"
-        })
-    }
-}
-const getQuestion = async (req, res) => {
-    try {
-        const {
-            id
-        } = req.body
-        if (!id) {
-            return res.status(404).send({
-                "status": "Failed",
-                "message": "Counldn't not find id"
-            })
-        }
-        const questions = await prismadb.question.findMany({
-            where: {
-                quizId: id
-            },
-            include: {
-                CorrectOption: true
-            }
-        })
-        return res.status(200).send({
-            "status": "success",
-            "question": questions
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error please try again"
-        })
-    }
-}
+  const { correctOption, id, OptionOne, OptionTwo, OptionThree, question } =
+    req.body;
+  if (
+    !correctOption ||
+    !OptionOne ||
+    !OptionTwo ||
+    !OptionThree ||
+    !id ||
+    !question
+  ) {
+    return res.send({
+      status: "Failed",
+      message: "please fill all field",
+    });
+    // checking if all fields are fields
+  }
+  if (
+    OptionOne === OptionTwo ||
+    OptionOne === OptionThree ||
+    OptionTwo === OptionThree
+  ) {
+    return res.status(400).send({
+      status: "Failed",
+      message: "Options must be unique",
+    });
+    // Options should be different from each other
+  }
+  if (
+    correctOption !== OptionOne &&
+    correctOption !== OptionTwo &&
+    correctOption !== OptionThree
+  ) {
+    return res.status(400).send({
+      status: "Failed",
+      message: "CorrectOption must be one of the provided options",
+    });
+    // Correct option must be same with one other option
+  }
+  try {
+    await prismadb.question.create({
+      data: {
+        quizId: id,
+        Question: question,
+        OptionOne: OptionOne,
+        OptionTwo: OptionTwo,
+        OptionThree: OptionThree,
+        CorrectOption: {
+          create: {
+            CorrectOption: correctOption,
+          },
+        },
+      },
+    });
+    return res.status(204).send();
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    return res.status(500).send({
+      status: "Failed",
+      message: "Internal server error please try again",
+    });
+  }
+};
 const deleteQuestion = async (req, res) => {
-    try {
-        const {
-            id,QuizId
-        } = req.body
-        if (!id) {
-            return res.send({
-                "status": "Failed",
-                "message": "Couldn't found the Question,Please try again"
-            })
-        }
-        await prismadb.correctOption.delete({
-            where: {
-                questionId: id
-            }
-        })
-        await prismadb.question.delete({
-            where: {
-                id,
-            },
-        })
-        const questions = await prismadb.question.findMany(
-            {
-                where:{
-                    quizId:QuizId,
-                }
-            }
-        )
-        return res.status(200).send({
-            "status": "success",
-            "question": questions,
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error, please try again1"
-        })
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.send({
+        status: "Failed",
+        message: "Please provide a valid question ID",
+      });
     }
-}
+
+    // Check if the question with the provided ID exists
+    const existingQuestion = await prismadb.question.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        CorrectOption: true,
+      },
+    });
+
+    if (!existingQuestion) {
+      return res.send({
+        status: "Failed",
+        message: "Question not found",
+      });
+    }
+
+    // Delete the correct option associated with the question
+    await prismadb.correctOption.delete({
+      where: {
+        questionId: id,
+      },
+    });
+
+    // Delete the question itself
+    await prismadb.question.delete({
+      where: {
+        id,
+      },
+    });
+
+    // Fetch updated list of questions
+    const questions = await prismadb.question.findMany({
+      where: {
+        quizId: existingQuestion.quizId,
+      },
+    });
+
+    return res.send({
+      status: "success",
+      message: "Question deleted successfully",
+      questions,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    res.status(500).send({
+      status: "Failed",
+      message: "Internal server error, please try again",
+    });
+  }
+};
+
+const getQuestion = async (req, res) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.status(404).send({
+        status: "Failed",
+        message: "Counldn't not find id",
+      });
+    }
+    const questions = await prismadb.question.findMany({
+      where: {
+        quizId: id,
+      },
+      include: {
+        CorrectOption: true,
+      },
+    });
+    return res.status(200).send({
+      status: "success",
+      question: questions,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    return res.status(500).send({
+      status: "Failed",
+      message: "Internal server error please try again",
+    });
+  }
+};
+
 const updateQuestion = async (req, res) => {
-    try {
-        const {
-            correctOption,
-            id,
-            OptionOne,
-            OptionTwo,
-            OptionThree,
-            question,
-            QuizId
-        } = req.body
-        if (!correctOption || !OptionOne || !OptionTwo || !OptionThree || !id || !question ||!QuizId) {
-            return res.send({
-                "status": "Failed",
-                "message": "please fill all fields"
-            })
-        }
-        await prismadb.correctOption.update({
-            where:{
-                questionId:id
-            },
-            data:{
-                CorrectOption:correctOption
-            }
-        })
-        await prismadb.question.update({
-            where: {
-                id
-            },
-            data: {
-                Question: question,
-                OptionOne: OptionOne,
-                OptionTwo: OptionTwo,
-                OptionThree: OptionThree,
-            }
-        })
-        const questions = await prismadb.question.findMany({
-            where:{
-                quizId:QuizId
-            }
-        })
-        return res.status(200).send({
-            "status": "success",
-            "question": questions,
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error, please try again"
-        })
+  try {
+    const {
+      correctOption,
+      id,
+      OptionOne,
+      OptionTwo,
+      OptionThree,
+      question,
+      QuizId,
+    } = req.body;
+    if (
+      !correctOption ||
+      !OptionOne ||
+      !OptionTwo ||
+      !OptionThree ||
+      !id ||
+      !question ||
+      !QuizId
+    ) {
+      return res.send({
+        status: "Failed",
+        message: "please fill all fields",
+      });
     }
-}
+    await prismadb.correctOption.update({
+      where: {
+        questionId: id,
+      },
+      data: {
+        CorrectOption: correctOption,
+      },
+    });
+    await prismadb.question.update({
+      where: {
+        id,
+      },
+      data: {
+        Question: question,
+        OptionOne: OptionOne,
+        OptionTwo: OptionTwo,
+        OptionThree: OptionThree,
+      },
+    });
+    const questions = await prismadb.question.findMany({
+      where: {
+        quizId: QuizId,
+      },
+    });
+    return res.status(200).send({
+      status: "success",
+      question: questions,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    return res.status(500).send({
+      status: "Failed",
+      message: "Internal server error, please try again",
+    });
+  }
+};
 module.exports = {
-    addQuestion,
-    getQuestion,
-    deleteQuestion,
-    updateQuestion,
-}
+  addQuestion,
+  getQuestion,
+  updateQuestion,
+  deleteQuestion,
+};
+
+// const deleteQuestion = async (req, res) => {
+//   try {
+//     const { id, QuizId } = req.body;
+//     if (!id) {
+//       return res.send({
+//         status: "Failed",
+//         message: "Couldn't found the Question,Please try again",
+//       });
+//     }
+//     await prismadb.correctOption.delete({
+//       where: {
+//         questionId: id,
+//       },
+//     });
+//     await prismadb.question.delete({
+//       where: {
+//         id,
+//       },
+//     });
+//     const questions = await prismadb.question.findMany({
+//       where: {
+//         quizId: QuizId,
+//       },
+//     });
+//     return res.status(200).send({
+//       status: "success",
+//       question: questions,
+//     });
+//   } catch (error) {
+//     console.log("----------------------------");
+//     console.log(error);
+//     console.log("---------------------------");
+//     res.status(500).send({
+//       status: "Failed",
+//       message: "Internal server error, please try again1",
+//     });
+//   }
+// };
--- src/controller/quizcontroller.js
+++ src/controller/quizcontroller.js
@@ -1,130 +1,262 @@
-const prismadb = require("../config/db.js")
+const prismadb = require("../config/db.js");
 const addQuiz = async (req, res) => {
-    const {
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.send({
+      status: "Failed",
+      message: "please fill all field",
+    });
+  }
+  try {
+    const quiz = await prismadb.quiz.create({
+      data: {
         title,
-        description
-    } = req.body
-    if (!title || !description) {
-        return res.send({
-            "status": "Failed",
-            "message": "please fill all field"
-        })
-    }
-    try {
-        const quiz = await prismadb.quiz.create({
-            data: {
-                title,
-                description,
-            }
-        })
-        return res.status(200).send({
-            "status": "success",
-            "quiz": quiz
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error please try again"
-        })
-    }
-
-}
+        description,
+      },
+    });
+    return res.status(200).send({
+      status: "success",
+      quiz: quiz,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    return res.status(500).send({
+      status: "Failed",
+      message: "Internal server error please try again",
+    });
+  }
+};
 const getQuiz = async (req, res) => {
-    try {
-        const quizs = await prismadb.quiz.findMany()
-        return res.status(200).send({
-            "status": "success",
-            "quizs": quizs
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error please try again"
-        })
-    }
-}
+  try {
+    const quizs = await prismadb.quiz.findMany();
+    return res.status(200).send({
+      status: "success",
+      quizs: quizs,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    return res.status(500).send({
+      status: "Failed",
+      message: "Internal server error please try again",
+    });
+  }
+};
 const deleteQuiz = async (req, res) => {
-    try {
-        const {
-            id
-        } = req.body
-        if (!id) {
-            return res.send({
-                "status": "Failed",
-                "message": "Couldn't found the Card,Please try again"
-            })
-        }
-        await prismadb.quiz.delete({
-            where: {
-                id
-            },
-            include: {
-                Question: true,
-                Result: true
-            },
-            
-        });
-        const quizs = await prismadb.quiz.findMany()
-        return res.send({
-            "status": "success",
-            "quiz": quizs,
-        });
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        return res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error, please try again1"
-        })
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res.send({
+        status: "Failed",
+        message: "Please provide a valid quiz ID",
+      });
     }
-}
+    const existingQuiz = await prismadb.quiz.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        Question: {
+          include: {
+            CorrectOption: true,
+          },
+        },
+        Result: {
+          include: {
+            ResultHistory: true,
+          },
+        },
+      },
+    });
+
+    if (!existingQuiz) {
+      return res.send({
+        status: "Failed",
+        message: "Quiz not found",
+      });
+    }
+    await Promise.all(
+      existingQuiz.Question.map(async (question) => {
+        await prismadb.correctOption.delete({
+          where: {
+            questionId: question.id,
+          },
+        });
+        return prismadb.question.delete({
+          where: {
+            id: question.id,
+          },
+        });
+      })
+    );
+
+    await Promise.all(
+      existingQuiz.Result.map(async (result) => {
+        await Promise.all(
+          result.ResultHistory.map(async (resultHistory) => {
+            return prismadb.resultHistory.delete({
+              where: {
+                id: resultHistory.id,
+              },
+            });
+          })
+        );
+        return prismadb.result.delete({
+          where: {
+            id: result.id,
+          },
+        });
+      })
+    );
+
+    await prismadb.quiz.delete({
+      where: {
+        id,
+      },
+    });
+
+    const quizs = await prismadb.quiz.findMany();
+
+    return res.send({
+      status: "success",
+      message: "Quiz and associated data deleted successfully",
+      quizs,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    res.status(500).send({
+      status: "Failed",
+      message: "Internal server error, please try again",
+    });
+  }
+};
+
 const updateQuiz = async (req, res) => {
-    try {
-        const {
-            id,
-            title,
-            description
-        } = req.body
-        if (!id || !title || !description) {
-            return res.send({
-                "status": "Failed",
-                "message": "please fill all fields"
-            })
-        }
-        const updatedquiz = await prismadb.quiz.update({
-            where: {
-                id,
-            },
-            data: {
-                title,
-                description,
-            }
-        })
-        const quizs = await prismadb.quiz.findMany()
-        return res.send({
-            "status": "success",
-            "quiz": quizs,
-        })
-    } catch (error) {
-        console.log("----------------------------");
-        console.log(error);
-        console.log("---------------------------");
-        res.status(500).send({
-            "status": "Failed",
-            "message": "Internal server error, please try again"
-        })
+  try {
+    const { id, title, description } = req.body;
+    if (!id || !title || !description) {
+      return res.send({
+        status: "Failed",
+        message: "please fill all fields",
+      });
     }
-}
+    const updatedquiz = await prismadb.quiz.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        description,
+      },
+    });
+    const quizs = await prismadb.quiz.findMany();
+    return res.send({
+      status: "success",
+      quiz: quizs,
+    });
+  } catch (error) {
+    console.log("----------------------------");
+    console.log(error);
+    console.log("---------------------------");
+    res.status(500).send({
+      status: "Failed",
+      message: "Internal server error, please try again",
+    });
+  }
+};
 module.exports = {
-    addQuiz,
-    getQuiz,
-    deleteQuiz,
-    updateQuiz,
-}
+  addQuiz,
+  getQuiz,
+  updateQuiz,
+  deleteQuiz,
+};
+
+// const deleteQuiz = async (req, res) => {
+//   try {
+//     const { id } = req.body;
+//     console.log("Received request to delete quiz with ID:", id);
+
+//     if (!id) {
+//       console.error("Invalid request: Quiz ID is missing.");
+//       return res.send({
+//         status: "Failed",
+//         message: "Couldn't find the quiz. Please provide a valid ID.",
+//       });
+//     }
+//     const resultData = await prismadb.result.findMany({
+//       where: {
+//         quizId: id,
+//       },
+//       select: {
+//         id: true,
+//       },
+//     });
+
+//     // Fetch resultIds associated with the quiz
+//     const resultIds = await prismadb.result
+//       .findMany({
+//         where: {
+//           quizId: id,
+//         },
+//         select: {
+//           id: true,
+//         },
+//       })
+//       .map((result) => result.id);
+
+//     console.log("Deleting resulthistory for quiz with ID:", id);
+//     await prismadb.resultHistory.deleteMany({
+//       where: {
+//         resultId: { in: resultIds },
+//       },
+//     });
+
+//     console.log("Deleting result for quiz with ID:", id);
+//     await prismadb.result.deleteMany({
+//       where: {
+//         quizId: id,
+//       },
+//     });
+
+//     console.log("Deleting questions for quiz with ID:", id);
+//     await prismadb.question.deleteMany({
+//       where: {
+//         quizId: id,
+//       },
+//     });
+
+//     console.log("Deleting quiz with ID:", id);
+//     await prismadb.quiz.delete({
+//       where: {
+//         id,
+//       },
+//     });
+
+//     const quizzes = await prismadb.quiz.findMany();
+//     console.log("Quiz deleted successfully.");
+//     return res.send({
+//       status: "success",
+//       quizs: resultIds,
+//     });
+//   } catch (error) {
+//     // console.error("Error during quiz deletion:", error);
+//     // return res.status(500).send({
+//     //   status: "Failed",
+//     //   message: "Internal server error. Please try again.",
+//     // });
+//     console.error("Error during quiz deletion:", error);
+
+//     // Log the detailed error message
+//     console.error(error);
+
+//     return res.status(500).send({
+//       status: "Failed",
+//       message: "Internal server error. Please try again.",
+//     });
+//   }
+// };
